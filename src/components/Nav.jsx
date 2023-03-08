094575ccfbec90c4676ed9ba2e3fd5fdd0c8d0cd
cabonillamo <carlobonilla640@gmail.com>
2023-03-08 15:31:27 -0600

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav id="nav" class="">
+      <ul id="links">
+        <li>
+          <a href="#inicio" class="seleccionado" onclick="seleccionar(this)">
+            INICIO
+          </a>
+        </li>
+        <li>
+          <a href="#sobremi" onclick="seleccionar(this)">
+            SOBRE MI
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" onclick="seleccionar(this)">
+            PORTFOLIO
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;

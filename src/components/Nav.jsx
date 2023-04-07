536b797361e2fa61924ac0763d2fec931be96d29
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-04-06 21:10:51 -0600

    Nav ready

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 function Nav() {
   return (
-    <nav id="nav" class="">
-      <ul id="links">
+    <nav id="nav" className="bg-gray-900 text-white px-4 py-2">
+      <ul id="links" className="flex justify-center gap-4">
         <li>
-          <a href="#inicio" class="seleccionado" onclick="seleccionar(this)">
+          <a href="#inicio" className="hover:text-gray-400">
             INICIO
           </a>
         </li>
         <li>
-          <a href="#sobremi" onclick="seleccionar(this)">
+          <a href="#sobremi" className="hover:text-gray-400">
             SOBRE MI
           </a>
         </li>
         <li>
-          <a href="#portfolio" onclick="seleccionar(this)">
+          <a href="#portfolio" className="hover:text-gray-400">
             PORTFOLIO
+          </a>
+        </li>
+        <li>
+          <a href="#linkedin" className="hover:text-gray-400">
+            <div className="flex items-center">
+              <FaLinkedin className="mr-2" />
+              <span>LinkedIn</span>
+            </div>
           </a>
         </li>
       </ul>

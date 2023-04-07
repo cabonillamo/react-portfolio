c2d89d158d49d37e6113ce3e0057cfe8b76c4b43
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-04-07 15:19:40 -0600

    Done the responsive mode

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,53 +1,51 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes, FaLinkedin } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Nav.css";
 
 function Nav() {
   const [showMenu, setShowMenu] = useState(false);
+  const [showCloseButton, setShowCloseButton] = useState(false);
 
   const handleMenuClick = () => {
     setShowMenu(!showMenu);
+    setShowCloseButton(!showCloseButton);
+  };
+
+  const handleCloseClick = () => {
+    setShowMenu(false);
+    setShowCloseButton(false);
   };
 
   return (
-    <nav
-      id="nav"
-      className="bg-gray-900 text-white px-4 py-2 flex justify-center"
-    >
-      <div className="flex items-center justify-between w-full max-w-screen-lg">
-        <a href="#inicio" className="hover:text-gray-400">
-          INICIO
-        </a>
+    <nav id="nav">
+      <div className="menu-container">
+        <a href="#inicio">INICIO</a>
         <button
-          className="md:hidden"
+          className={`btn-menu d-lg-none ${
+            showCloseButton ? "hidden" : "block"
+          }`}
           onClick={handleMenuClick}
           aria-label="Toggle navigation"
         >
-          {showMenu ? <FaTimes /> : <FaBars />}
+          <FaBars />
         </button>
-        <ul
-          id="links"
-          className={`${
-            showMenu ? "flex-col" : "hidden"
-          } md:flex md:flex-row md:justify-center md:gap-4 mt-4 md:mt-0 flex-col items-center`}
-        >
+        <ul className={`d-md-flex ${showMenu ? "show" : "hidden"}`}>
           <li>
-            <a href="#sobremi" className="hover:text-gray-400">
-              SOBRE MI
-            </a>
+            <a href="#sobremi">SOBRE MI</a>
           </li>
           <li>
-            <a href="#portfolio" className="hover:text-gray-400">
-              PORTFOLIO
-            </a>
+            <a href="#portfolio">PORTFOLIO</a>
           </li>
           <li>
-            <a href="#linkedin" className="hover:text-gray-400">
-              <div className="flex items-center">
-                <FaLinkedin className="mr-2" />
-                <span>LinkedIn</span>
-              </div>
-            </a>
+            <a href="#linkedin">LinkedIn</a>
           </li>
+          <button
+            className="close-button d-lg-none"
+            onClick={handleCloseClick}
+            aria-label="Close navigation"
+          >
+            <FaTimes />
+          </button>
         </ul>
       </div>
     </nav>
